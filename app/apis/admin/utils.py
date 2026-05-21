e8e8e636bc7e1a213bc62f88088ff3310544b3e4
arturoandresv <arturovelasquezo0711@gmail.com>
2026-05-21 15:48:44 -0500

    Revert "fix: remote code execution mitigation"
    This reverts commit 9fcba84530db95cf9860c31a5860a6c6dc1f12af.

--- app/apis/admin/utils.py
+++ app/apis/admin/utils.py
@@ -1,23 +1,15 @@
 import subprocess
 
-#Definir una lista de parametros de df permitidos
-ALLOWED_DF_ARGS = {"-h","-T","-a","--total"}
 
 def get_disk_usage(parameters: str):
-    
-    #Permite solo los parametros dentro de la lista
-    safe_args = [arg for arg in parameters.split() if arg in ALLOWED_DF_ARGS]
-    
-    #Construye el comando mediante una lista
-    command = "df" + safe_args
+    command = "df -h " + parameters
 
     try:
         result = subprocess.run(
-            command, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+            command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
         )
         usage = result.stdout.strip().decode()
     except:
         raise Exception("An unexpected error was observed")
 
     return usage
-
